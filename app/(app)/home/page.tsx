fdf972618dedabe5c44690deb737b5664195d79a
Priyam216 <spriyam2002@gmail.com>
2024-08-25 21:20:00 +0530

    Added Home Screen component

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+"use client"
+import React, {useState, useEffect, useCallback} from 'react'
+import axios from 'axios'
+import VideoCard from '@/components/VideoCard'
+import { Video } from '@prisma/client'
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    const [videos, setVideos] = useState<Video[]>([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
+
+    const fetchVideos = useCallback(async () => {
+        try {
+            const response = await axios.get("/api/videos")
+            if(Array.isArray(response.data)) {
+                setVideos(response.data)
+            } else {
+                throw new Error(" Unexpected response format");
+
+            }
+        } catch (error) {
+            console.log(error);
+            setError("Failed to fetch videos")
+
+        } finally {
+            setLoading(false)
+        }
+    }, [])
+
+    useEffect(() => {
+        fetchVideos()
+    }, [fetchVideos])
+
+    const handleDownload = useCallback((url: string, title: string) => {
+        () => {
+            const link = document.createElement("a");
+            link.href = url;
+            link.setAttribute("download", `${title}.mp4`);
+            link.setAttribute("target", "_blank");
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+        }
+
+    }, [])
+
+    if(loading){
+        return <div>Loading...</div>
+    }
+
+    return (
+        <div>Home</div>
+      );
 }
 
 export default Home
